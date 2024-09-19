babae685bc1bccb8eb9c2cfde2839f66a7b827b3
Lasithamd <lasithamadhura@gmail.com>
2024-09-19 14:26:59 +0530

    Connection Test Done

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql2')
+var bodyParser = require('body-parser')
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'course_manage'
 })
+
+connection.connect()
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/students', (req, res) => {
+  const sql='SELECT * from students'
+  connection.query(sql, (err, rows, fields) => {
+    if (err) throw err
+    res.send(rows)
+  })
+  
+})
+
+app.post('/students', (req, res) => {
+  const re= req.body
+  const sql = 'insert into students (name,phone,email,app_password)values (?,?,?,?)'
+  connection.query(sql,[re.name,re.phone,re.email,re.app_password], (err, rows, fields) => {
+    if (err) throw err
+    res.send('Student added successfull...!')
+  })
+})
+
+app.put('/students/:id', (req, res) => {
+  const re= req.body
+  const sql = 'insert into students (name,phone,email,app_password)values (?,?,?,?)'
+  connection.query(sql,[re.name,re.phone,re.email,re.app_password], (err, rows, fields) => {
+    if (err) throw err
+    res.send('Student added success...!')
+  })
+})
+
 
 
 app.listen(port, () => {
