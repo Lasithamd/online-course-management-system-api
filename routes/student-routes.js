468c06bf15ce21c008df2608a83a80be3dc2622c
Lasitha_madhuranga <lasithadayarathna@gmail.com>
2024-10-13 22:33:04 +0530

    student register Updated

--- routes/student-routes.js
+++ routes/student-routes.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const {getStudent, saveStudent, updateStudent, deleteStudent,getSingleStudent} =require('../controller/student-controller')
+const {getStudent, saveStudent, updateStudent, deleteStudent,getSingleStudent,login} =require('../controller/student-controller')
 
 router.get('/', getStudent)
 router.get('/:id', getSingleStudent)
 router.post('/', saveStudent)
 router.put('/:id', updateStudent)
 router.delete('/:id', deleteStudent)
+router.post('/login', saveStudent)
 
 module.exports = router
